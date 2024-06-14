1a8f0d7ad4dee889701e717365793151e40e930d
고경우 <dfdfg11@gmail.com>
2024-06-14 15:10:15 +0900

    [FEAT] 텍스트 추출 기능

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Todo from './Todo';
 import AddTodo from './AddTodo';
-import { Container, Grid, AppBar, Toolbar, Typography, Paper, List, IconButton, TextField } from "@material-ui/core";
+import { Container, Grid, AppBar, Toolbar, Typography, Paper, List, IconButton, TextField, Button } from "@material-ui/core";
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import Pagination from '@mui/material/Pagination';
@@ -11,165 +11,171 @@ import Clear from './Clear';
 import WeatherWidget from './WeatherWidget';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: [],
-      loading: true,
-      date: new Date(), // 날짜 상태 추가
-      page: 1, // 현재 페이지
-      itemsPerPage: 5, // 페이지당 항목 수
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [],
+            loading: true,
+            date: new Date(),
+            page: 1,
+            itemsPerPage: 5,
+        };
+    }
 
-  add = (item) => {
-    call("/todo", "POST", item).then((response) =>
-      this.setState({ items: response.data })
-    );
-  }
-
-  delete = (item) => {
-    call("/todo", "DELETE", item).then((response) =>
-      this.setState({ items: response.data })
-    );
-  }
-
-  clearAllDonelist = () => {
-    const thisItems = this.state.items;
-    thisItems.forEach((tdl) => {
-      if (tdl.done === true) {
-        call("/todo", "DELETE", tdl).then((response) =>
-          this.setState({ items: response.data })
+    add = (item) => {
+        call("/todo", "POST", item).then((response) =>
+            this.setState({ items: response.data })
         );
-      }
-    });
-  }
+    }
 
-  clearAll = () => {
-    const thisItems = this.state.items;
-    thisItems.forEach((tdl) => {
-      call("/todo", "DELETE", tdl).then((response) =>
-        this.setState({ items: response.data })
-      );
-    });
-  }
+    delete = (item) => {
+        call("/todo", "DELETE", item).then((response) =>
+            this.setState({ items: response.data })
+        );
+    }
 
-  update = (item) => {
-    call("/todo", "PUT", item).then((response) =>
-      this.setState({ items: response.data })
-    );
-  }
+    clearAllDonelist = () => {
+        const thisItems = this.state.items;
+        thisItems.forEach((tdl) => {
+            if (tdl.done === true) {
+                call("/todo", "DELETE", tdl).then((response) =>
+                    this.setState({ items: response.data })
+                );
+            }
+        });
+    }
 
-  componentDidMount() {
-    call("/todo", "GET", null).then((response) =>
-      this.setState({ items: response.data, loading: false })
-    );
-  }
+    clearAll = () => {
+        const thisItems = this.state.items;
+        thisItems.forEach((tdl) => {
+            call("/todo", "DELETE", tdl).then((response) =>
+                this.setState({ items: response.data })
+            );
+        });
+    }
 
-  handleDateChange = (date) => {
-    this.setState({ date });
-  }
+    update = (item) => {
+        call("/todo", "PUT", item).then((response) =>
+            this.setState({ items: response.data })
+        );
+    }
 
-  handlePageChange = (event, value) => {
-    this.setState({ page: value });
-  }
+    componentDidMount() {
+        call("/todo", "GET", null).then((response) =>
+            this.setState({ items: response.data, loading: false })
+        );
+    }
 
-  render() {
-    const { items, date, page, itemsPerPage } = this.state;
+    handleDateChange = (date) => {
+        this.setState({ date });
+    }
 
-    // 메인 할 일 필터링
-    const mainTasks = items.filter(item => item.isMainTask);
-    
-    // 선택한 날짜의 할일만 필터링
-    const filteredItems = items.filter(item => {
-      const itemDate = new Date(item.date);
-      return itemDate.toDateString() === date.toDateString();
-    });
+    handlePageChange = (event, value) => {
+        this.setState({ page: value });
+    }
 
+    exportTodos = () => {
+        const filePath = 'todos.txt';
+        call(`/todo/export?filePath=${filePath}`, "POST", null)
+            .then(() => console.log('Todos exported'))
+            .catch(error => console.error('Error exporting todos:', error));
+    }
 
+    render() {
+        const { items, date, page, itemsPerPage } = this.state;
 
-    // 페이지네이션을 위한 항목 분할
-    const startIndex = (page - 1) * itemsPerPage;
-    const paginatedItems = filteredItems.slice(startIndex, startIndex + itemsPerPage);
+        // 메인 할 일 필터링
+        const mainTasks = items.filter(item => item.isMainTask);
 
-    const todoItems = paginatedItems.length > 0 ? (
-      <div className="lists">
-        <List>
-          {paginatedItems.map((item, idx) => (
-            <Todo item={item} key={item.id} delete={this.delete} update={this.update} />
-          ))}
-        </List>
-      </div>
-    ) : (
-      <p>선택한 날짜에 할일이 없습니다.</p>
-    );
+        // 선택한 날짜의 할일만 필터링
+        const filteredItems = items.filter(item => {
+            const itemDate = new Date(item.date);
+            return itemDate.toDateString() === date.toDateString();
+        });
 
-    const navigationBar = (
-      <AppBar position="static" style={{ height: 60 }}>
-        <Toolbar style={{ minHeight: 50 }}>
-          <Grid justifyContent="space-between" container>
-            <Grid item>
-              <Typography variant="h6">Today quest</Typography>
-            </Grid>
-            <Grid item>
-              <IconButton color="inherit" onClick={signout}>로그아웃</IconButton>
-            </Grid>
-          </Grid>
-        </Toolbar>
-      </AppBar>
-    );
+        // 페이지네이션을 위한 항목 분할
+        const startIndex = (page - 1) * itemsPerPage;
+        const paginatedItems = filteredItems.slice(startIndex, startIndex + itemsPerPage);
 
-    const todoListPage = (
-      <div>
-        {navigationBar}
-        <Container maxWidth="md">
-          <WeatherWidget />
-          <LocalizationProvider dateAdapter={AdapterDateFns}>
-            <DatePicker
-              label="Select Date"
-              value={this.state.date}
-              onChange={this.handleDateChange}
-              renderInput={(params) => <TextField {...params} fullWidth />}
-            />
-          </LocalizationProvider>
-          <AddTodo add={this.add} />
-          <div className="TodoList">
-            {/* 메인 할 일을 항상 표시 */}
-            <Paper style={{ margin: 16 }}>
-              <Typography variant="h6" style={{ margin: 16 }}>Main Tasks</Typography>
-              <List>
-                {mainTasks.map((item, idx) => (
-                  <Todo item={item} key={item.id} delete={this.delete} update={this.update} />
-                ))}
-              </List>
-            </Paper>
-            <Paper style={{ margin: 16 }}>
-              <Typography variant="h6" style={{ margin: 16 }}>Tasks for {date.toDateString()}</Typography>
-              {todoItems}
-              <Pagination
-                count={Math.ceil(filteredItems.length / itemsPerPage)}
-                page={page}
-                onChange={this.handlePageChange}
-                color="primary"
-                style={{ display: 'flex', justifyContent: 'center', marginTop: 16 }}
-              />
-            </Paper>
-          </div>
-        </Container>
-        <DeleteDoneAll clearAllDonelist={this.clearAllDonelist} />
-        <Clear clearAll={this.clearAll} />
-      </div>
-    );
+        const todoItems = paginatedItems.length > 0 ? (
+            <div className="lists">
+                <List>
+                    {paginatedItems.map((item, idx) => (
+                        <Todo item={item} key={item.id} delete={this.delete} update={this.update} />
+                    ))}
+                </List>
+            </div>
+        ) : (
+            <p>선택한 날짜에 할일이 없습니다.</p>
+        );
 
-    const loadingPage = <h1>Loading...</h1>
-    const content = this.state.loading ? loadingPage : todoListPage;
+        const navigationBar = (
+            <AppBar position="static" style={{ height: 60 }}>
+                <Toolbar style={{ minHeight: 50 }}>
+                    <Grid justifyContent="space-between" container>
+                        <Grid item>
+                            <Typography variant="h6">Today quest</Typography>
+                        </Grid>
+                        <Grid item>
+                            <IconButton color="inherit" onClick={signout}>로그아웃</IconButton>
+                        </Grid>
+                    </Grid>
+                </Toolbar>
+            </AppBar>
+        );
 
-    return (
-      <div className="App">
-        {content}
-      </div>
-    );
-  }
+        const todoListPage = (
+            <div>
+                {navigationBar}
+                <Container maxWidth="md">
+                    <WeatherWidget />
+                    <LocalizationProvider dateAdapter={AdapterDateFns}>
+                        <DatePicker
+                            label="Select Date"
+                            value={this.state.date}
+                            onChange={this.handleDateChange}
+                            renderInput={(params) => <TextField {...params} fullWidth />}
+                        />
+                    </LocalizationProvider>
+                    <AddTodo add={this.add} />
+                    <div className="TodoList">
+                        {/* 메인 할 일을 항상 표시 */}
+                        <Paper style={{ margin: 16 }}>
+                            <Typography variant="h6" style={{ margin: 16 }}>Main Tasks</Typography>
+                            <List>
+                                {mainTasks.map((item, idx) => (
+                                    <Todo item={item} key={item.id} delete={this.delete} update={this.update} />
+                                ))}
+                            </List>
+                        </Paper>
+                        <Paper style={{ margin: 16 }}>
+                            <Typography variant="h6" style={{ margin: 16 }}>Tasks for {date.toDateString()}</Typography>
+                            {todoItems}
+                            <Pagination
+                                count={Math.ceil(filteredItems.length / itemsPerPage)}
+                                page={page}
+                                onChange={this.handlePageChange}
+                                color="primary"
+                                style={{ display: 'flex', justifyContent: 'center', marginTop: 16 }}
+                            />
+                        </Paper>
+                    </div>
+                </Container>
+                <DeleteDoneAll clearAllDonelist={this.clearAllDonelist} />
+                <Clear clearAll={this.clearAll} />
+                <Button onClick={this.exportTodos}>Export Todos</Button>
+            </div>
+        );
+
+        const loadingPage = <h1>Loading...</h1>
+        const content = this.state.loading ? loadingPage : todoListPage;
+
+        return (
+            <div className="App">
+                {content}
+            </div>
+        );
+    }
 }
 
 export default App;
